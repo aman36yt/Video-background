2901263bdee08427dcdc7d6d6551e5f2ab1633f8
Aman <amanbhai36yt@gmail.com>
2026-03-28 22:24:00 +0530

    functionality Updated

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const btn = document.querySelector(".btn");
 const video = document.querySelector(".back-vid");
 const icon = document.querySelector(".fa");
+const preloader = document.querySelector(".preloader");
 
 btn.addEventListener("click",()=>{
     if(btn.classList.contains("pause")){
         btn.classList.remove("pause");
         icon.classList.remove("fa-pause");
         icon.classList.add("fa-play");
-        video.play();
+        video.play(); //INbuilt function to play video
     }else{
         btn.classList.add("pause");
         icon.classList.remove("fa-play");
         icon.classList.add("fa-pause");
-        video.pause();
+        video.pause(); //Inbuilt Function to Pause video
     }
+});
+
+window.addEventListener("load",()=>{
+    preloader.style.zIndex = "-2";
 });
